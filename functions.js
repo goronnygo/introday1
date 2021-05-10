26b2d5253f96d9c9e0c612fca92ef4deabcb469e
Ronny <77699546+goronnygo@users.noreply.github.com>
2021-05-10 11:48:21 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,85 @@
+const returnEmptyString = function () {
+    return "";
+};
+
+const returnZeroNumber = function () {
+    return 0;
+};
+
+const returnEmptyArray = function () {
+    return [];
+};
+
+const returnEmptyObject = function () {
+    return {};
+};
+
+const returnAbcString = function (value) {
+    return "abc123";
+};
+
+const returnSumOfTen = function (arg1, arg2) {
+    return (arg1 + arg2);
+};
+
+const subtraction = function (arg1, arg2) {
+    return (arg1 - arg2);
+};
+
+const multiplication = function (arg1, arg2) {
+    return (arg1 * arg2);
+};
+
+const division = function (arg1, arg2) {
+    return (arg1 / arg2);
+};
+
+const returnArray = function (arg1) {
+    return arg1;
+};
+
+const returnFirstIndex = function (arg1) {
+    return arg1[0];
+};
+
+const returnSecondIndex = function (arg1) {
+    return arg1[1];
+};
+
+const returnArrayLength = function (arg1) {
+    return arg1[2];
+};
+
+const arraySum = function (arg1) {
+    let sum = 0
+    for (let i of arg1){
+        sum += i
+    }
+    return sum;
+};
+
+const arraySubtraction = function (arg1) {
+    let sum = 0
+    for (let i of arg1){
+        sum -= i
+    }
+    return sum;
+};
+
+const multiplicationArray = function (arg1) {
+};
+
+const divisionArray = function (arg1) {
+
+};
+
+const oddArray = function (arg1) {
+
+};
+
+const evenArray = function (arg1) {
+
+};
+
+const returnFruits = function (arg1) {
+};
